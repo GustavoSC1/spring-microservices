dd99d4959ca83f660068258d928241c3de77ce8a
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-10-10 21:00:49 -0300

    Updating Settings without Interrupting the Microservice

--- greeting-service/src/main/java/com/gustavo/greetingservice/configuration/GreetingConfiguration.java
+++ greeting-service/src/main/java/com/gustavo/greetingservice/configuration/GreetingConfiguration.java
@@ -1,9 +1,13 @@
 package com.gustavo.greetingservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+// É usado para carregar as novas propriedades de configuração do servidor Config sem precisar reiniciar o aplicativo e sem qualquer 
+// tempo de inatividade. (/actuator/refresh)
+@RefreshScope
 // Permite mapear facilmente todos os arquivos de Properties e Yaml em um objeto. 
 @ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
