f4577404692b29a8f983905785c65e000595b284
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-10-11 23:14:48 -0300

    Creating the Cambio Service Microservice

---
+++ cambio-service/src/test/java/com/gustavo/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
