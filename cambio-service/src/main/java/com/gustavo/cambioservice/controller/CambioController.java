6033cdda4c9ae7818719b71ad7f7bede5a1125bf
Gustavo Cruz <gu.cruz17@hotmail.com>
2022-10-12 00:59:11 -0300

    Retrieving the Running Instance Port

--- cambio-service/src/main/java/com/gustavo/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/gustavo/cambioservice/controller/CambioController.java
@@ -2,6 +2,8 @@ package com.gustavo.cambioservice.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,19 @@ import com.gustavo.cambioservice.model.Cambio;
 @RequestMapping("cambio-service")
 public class CambioController {
 	
+	// Permite acessar diversas propriedades da aplicação
+	@Autowired
+	private Environment enviroment;
+	
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(
 			@PathVariable("amount") BigDecimal amount,
 			@PathVariable("from") String from,
 			@PathVariable("to") String to) {
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+				
+		var port = enviroment.getProperty("local.server.port");
+		
+		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 	
 }
