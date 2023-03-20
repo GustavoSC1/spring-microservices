eeb76814d49ca21b6ce2edfa51fac5732b44c712
Gustavo Cruz <gu.cruz17@hotmail.com>
2023-03-20 20:26:07 -0300

    Fallback Methods with Resilience4j Wait Duration and Exponential Backoff

--- book-service/src/main/java/com/gustavo/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/gustavo/bookservice/controller/FooBarController.java
@@ -16,13 +16,20 @@ public class FooBarController {
 	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 	
 	@GetMapping("/foo-bar")
-	@Retry(name = "foo-bar")// Repite as chamadas sempre que elas falharem.
+	// Repite as chamadas sempre que elas falharem, durante uma 'x' quantidade de vezes e um intervalo 'y' de tempo.
+	// O mecanismo do método de fallback funciona como um bloco try/catch. Se um método de fallback for configurado, 
+	// cada exceção será encaminhada para um executor de método de fallback.
+	@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
 	public String fooBar() {
 		logger.info("Request to foo-bar is received!");
 		var response = new RestTemplate()
 				.getForEntity("http://localhost:8080/foo-bar", String.class); // Faz uma solicitação pra um endereço inexistente
 		// return "Foo-Bar!!!";
 		return response.getBody();		
+	}
+	
+	public String fallbackMethod(Exception ex) {
+		return "fallbackMethod foo-bar!!!";
 	}
 
 }
