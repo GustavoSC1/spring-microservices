b2fbf38ccc445324f7a7fa5ab6cf9b7c91db2946
Gustavo Cruz <gu.cruz17@hotmail.com>
2023-03-20 18:11:00 -0300

    Retry with Resilience4j

--- book-service/src/main/java/com/gustavo/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/gustavo/bookservice/controller/FooBarController.java
@@ -1,16 +1,28 @@
 package com.gustavo.bookservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
 	
+	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+	
 	@GetMapping("/foo-bar")
+	@Retry(name = "foo-bar")// Repite as chamadas sempre que elas falharem.
 	public String fooBar() {
-		return "Foo-Bar!!!";
+		logger.info("Request to foo-bar is received!");
+		var response = new RestTemplate()
+				.getForEntity("http://localhost:8080/foo-bar", String.class); // Faz uma solicitação pra um endereço inexistente
+		// return "Foo-Bar!!!";
+		return response.getBody();		
 	}
 
 }
