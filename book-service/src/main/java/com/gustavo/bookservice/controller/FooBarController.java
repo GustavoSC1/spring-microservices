aafba436b374a984cf8e115c7dc45b543e1b3eca
Gustavo Cruz <gu.cruz17@hotmail.com>
2023-03-21 00:26:49 -0300

    Knowing Resilience4j's Circuit Breaker Features

--- book-service/src/main/java/com/gustavo/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/gustavo/bookservice/controller/FooBarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -14,18 +15,21 @@ import io.github.resilience4j.retry.annotation.Retry;
 public class FooBarController {
 	
 	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
-	
-	@GetMapping("/foo-bar")
+		
 	// Repite as chamadas sempre que elas falharem, durante uma 'x' quantidade de vezes e um intervalo 'y' de tempo.
 	// O mecanismo do método de fallback funciona como um bloco try/catch. Se um método de fallback for configurado, 
 	// cada exceção será encaminhada para um executor de método de fallback.
-	@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+	// @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+	@GetMapping("/foo-bar")
+	// https://reflectoring.io/circuitbreaker-with-resilience4j/
+	// Um padrão comum ao usar Circuit Breaker é especificar um método de fallback a ser chamado quando o circuito estiver aberto.
+	@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
 	public String fooBar() {
 		logger.info("Request to foo-bar is received!");
 		var response = new RestTemplate()
 				.getForEntity("http://localhost:8080/foo-bar", String.class); // Faz uma solicitação pra um endereço inexistente
 		// return "Foo-Bar!!!";
-		return response.getBody();		
+		return response.getBody();
 	}
 	
 	public String fallbackMethod(Exception ex) {
