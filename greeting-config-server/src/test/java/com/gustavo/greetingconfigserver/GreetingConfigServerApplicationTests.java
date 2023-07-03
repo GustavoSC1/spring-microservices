d2b17eec06982f5c6aa7a3037c969d4eed17d013
Gustavo Cruz <gu.cruz17@hotmail.com>
2023-07-03 09:30:19 -0300

    Changing config-server to greeting-config-server

--- greeting-config-server/src/test/java/com/gustavo/greetingconfigserver/GreetingConfigServerApplicationTests.java
+++ greeting-config-server/src/test/java/com/gustavo/greetingconfigserver/GreetingConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.gustavo.configserver;
+package com.gustavo.greetingconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+class GreetingConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
